a943af90329e690a1668ca4f87d9329035b7d243
Ian Kenneth Navia <iannavia14@gmail.com>
2026-03-03 06:39:36 +0800

    clean code using arrow functions

--- rpsChallenge.js
+++ rpsChallenge.js
@@ -9,9 +9,7 @@ function getComputerChoice() {
 
 //player side
 //return players input
-function getPlayerInput() {
-  return prompt("Enter Rock or Paper or Scissors");
-}
+const getPlayerInput = () => prompt("Enter Rock or Paper or Scissors");
 
 //return getPlayerInput case-insensitve
 function getPlayerChoice(input) {
@@ -30,47 +28,44 @@ function getPlayerChoice(input) {
     }
 }
 
-function getValidPlayerChoice() {
-  return getPlayerChoice(getPlayerInput());
-}
+const getValidPlayerChoice = () => getPlayerChoice(getPlayerInput());
 
 function playGame() {
   let playerScore = 0;
   let computerScore = 0;
 
-  //Play and scoring
-  function playRound(playerChoice, computerChoice) {
-    console.log("Player choose   : " + playerChoice);
-    console.log("Computer choose : " + computerChoice);
-    if (!playerChoice) {
-      console.log("Invalid input. Round skipped.");
-      return;
-    }
-    else if (playerChoice === computerChoice) {
-      console.log("It's a tie!");
-    } else if (
-      (playerChoice === "rock" && computerChoice === "scissors") ||
-      (playerChoice === "scissors" && computerChoice === "paper") ||
-      (playerChoice === "paper" && computerChoice === "rock")
-    ) {
-      console.log(`You Win (${playerChoice} beats ${computerChoice})`);
-      playerScore++;
-    } else {
-      console.log(`Computer Win(${computerChoice} beats ${playerChoice})`);
-      computerScore++;
-    }
-    console.log(`Score: Player: ${playerScore} / Computer: ${computerScore}`);
+//Play and scoring
+function playRound(playerChoice, computerChoice) {
+  console.log("Player choose   : " + playerChoice);
+  console.log("Computer choose : " + computerChoice);
+  if (!playerChoice) {
+    console.log("Invalid input. Round skipped.");
     console.log("");
+    return;
+  } else if (playerChoice === computerChoice)console.log("It's a tie!");
+    else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "scissors" && computerChoice === "paper") ||
+    (playerChoice === "paper" && computerChoice === "rock")
+  ) {
+    console.log(`You Win (${playerChoice} beats ${computerChoice})`);
+    playerScore++;
+  } else {
+    console.log(`Computer Win(${computerChoice} beats ${playerChoice})`);
+    computerScore++;
   }
+  console.log(`Score: Player: ${playerScore} / Computer: ${computerScore}`);
+  console.log("");
+}
 
-  //5 rounds
+//5 rounds
   playRound(getValidPlayerChoice(), getComputerChoice());
   playRound(getValidPlayerChoice(), getComputerChoice());
   playRound(getValidPlayerChoice(), getComputerChoice());
   playRound(getValidPlayerChoice(), getComputerChoice());
   playRound(getValidPlayerChoice(), getComputerChoice());
 
-  // Final result
+// Final result
   console.log("Final Result:");
   if (playerScore > computerScore) console.log("You win the game!");
   else if (computerScore > playerScore) console.log("Computer wins the game!");
